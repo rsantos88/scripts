78394970fc304956afc89dfd2ac2148d59e666a3
Raúl de Santos Rico <rasantos@it.uc3m.es>
2022-11-28 13:21:42 +0100

    modified joints number for real robot

--- bash-scripts/teo-homing.sh
+++ bash-scripts/teo-homing.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "1) Configuring Teo to position mode"
-echo "set icmd cmds (pos pos pos pos pos pos pos)" | yarp rpc /teo/rightArm/rpc:i
-echo "set icmd cmds (pos pos pos pos pos pos pos)" | yarp rpc /teo/leftArm/rpc:i
-echo "set icmd cmds (pos pos pos pos pos pos)" | yarp rpc /teo/rightLeg/rpc:i
-echo "set icmd cmds (pos pos pos pos pos pos)" | yarp rpc /teo/leftLeg/rpc:i
+#echo "set icmd cmds (pos pos pos pos pos pos)" | yarp rpc /teo/rightArm/rpc:i
+#echo "set icmd cmds (pos pos pos pos pos pos)" | yarp rpc /teo/leftArm/rpc:i
+#echo "set icmd cmds (pos pos pos pos pos pos)" | yarp rpc /teo/rightLeg/rpc:i
+#echo "set icmd cmds (pos pos pos pos pos pos)" | yarp rpc /teo/leftLeg/rpc:i
 echo "2) Doing home position of arms and legs"
-echo "set poss (0 0 0 0 0 0 0)" |  yarp rpc /teo/rightArm/rpc:i
-echo "set poss (0 0 0 0 0 0 0)" |  yarp rpc /teo/leftArm/rpc:i
-echo "set poss (0 0 0 0 0 0)" |  yarp rpc /teo/rightLeg/rpc:i
-echo "set poss (0 0 0 0 0 0)" |  yarp rpc /teo/leftLeg/rpc:i
+echo "set poss (0 0 0 0 0 0)" |  yarp rpc /teo/rightArm/rpc:i
+echo "set poss (0 0 0 0 0 0)" |  yarp rpc /teo/leftArm/rpc:i
+#echo "set poss (0 0 0 0 0 0)" |  yarp rpc /teo/rightLeg/rpc:i
+#echo "set poss (0 0 0 0 0 0)" |  yarp rpc /teo/leftLeg/rpc:i
