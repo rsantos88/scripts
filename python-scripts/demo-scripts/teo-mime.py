c98d0414b553728df9686b163d7b79e3721c7aa4
Raúl de Santos Rico <rasantos@it.uc3m.es>
2022-11-29 14:08:17 +0100

    updated teo-mime with espeakClientto call checkSayDone()

--- python-scripts/demo-scripts/teo-mime.py
+++ python-scripts/demo-scripts/teo-mime.py
@@ -7,17 +7,18 @@
 # CopyPolicy: released under the terms of the LGPLv2.1
 # Python version: 2.7
 
-robot = '/teo'
+robot = '/teoSim'
 
 from time import sleep
+import roboticslab_speech as speech
 
-import thread
+import _thread
 
 # YARP
 import yarp  # imports YARP
 yarp.Network.init()  # connect to YARP network
 if yarp.Network.checkNetwork() != True:  # let's see if there was actually a reachable YARP network
-    print '[error] Please try running yarp server'  # tell the user to start one with 'yarp server' if there isn't any
+    print ('[error] Please try running yarp server')  # tell the user to start one with 'yarp server' if there isn't any
     quit()
 
 #-- Left Arm (LA)
@@ -27,6 +28,7 @@ optionsLA.put('remote',robot+'/leftArm')  # we add info on to whom we will conne
 optionsLA.put('local','/demo'+robot+'/leftArm')  # we add info on how we will call ourselves on the YARP network
 ddLA = yarp.PolyDriver(optionsLA)  # create a YARP multi-use driver with the given options
 posLA = ddLA.viewIPositionControl()  # make a position controller object we call 'pos'
+llLA = ddLA.viewIControlLimits() # ***********
 axesLA = posLA.getAxes()  # retrieve number of joints
 
 #-- Right Arm (RA)
@@ -36,6 +38,7 @@ optionsRA.put('remote',robot+'/rightArm')  # we add info on to whom we will conn
 optionsRA.put('local','/demo'+robot+'/rightArm')  # we add info on how we will call ourselves on the YARP network
 ddRA = yarp.PolyDriver(optionsRA)  # create a YARP multi-use driver with the given options
 posRA = ddRA.viewIPositionControl()  # make a position controller object we call 'pos'
+llRA = ddRA.viewIControlLimits() # ***********
 axesRA = posRA.getAxes()  # retrieve number of joints
 
 #-- HEAD (H)
@@ -48,23 +51,22 @@ posH = ddH.viewIPositionControl()  # make a position controller object we call '
 axesH = posH.getAxes()  # retrieve number of joints
 
 #-- Text-to-speech (TTS)
-tts = yarp.RpcClient()
-tts.open('/demo/tts/rpc:c')
-yarp.Network.connect('/demo/tts/rpc:c','/tts/rpc:s');
+client = yarp.RpcClient()
 
-def ttsLang(language):
-    cmd = yarp.Bottle()
-    res = yarp.Bottle()
-    cmd.addString('setLanguage')
-    cmd.addString(language)
-    tts.write(cmd,res)
+if not client.open('/demo/tts/rpc:c'):
+    print('Unable to open client port %s' % client.getName())
+    raise SystemExit
 
-def ttsSay(sayStr):
-    cmd = yarp.Bottle()
-    res = yarp.Bottle()
-    cmd.addString('say')
-    cmd.addString(sayStr)
-    tts.write(cmd,res)
+if not yarp.Network.connect('/demo/tts/rpc:c','/tts/rpc:s'):
+    print('Unable to connect to remote server port %s' % args.remote)
+    raise SystemExit
+
+tts = speech.TextToSpeechIDL()
+tts.yarp().attachAsClient(client)
+
+tts.setLanguage('mb-es1')
+tts.setSpeed(150) # Values 80 to 450.
+tts.setPitch(60) # 50 = normal
 
 
 #-- Pause rutine
@@ -85,12 +87,14 @@ for joint in range(0, axesRA):
 	posLA.setRefAcceleration(joint, 20)
 
 # Configure speed
+
+for joint in range(0, axesRA):
+	llRA.setVelLimits(joint, 0, 35) 
+	llLA.setVelLimits(joint, 0, 35)
+	
 sp = yarp.DVector(axesLA, 30)
 posRA.setRefSpeeds(sp)
 posLA.setRefSpeeds(sp)
-
-# Configure speech
-ttsLang('mb-es1')
 
 # Home
 
@@ -103,15 +107,15 @@ la = yarp.DVector(axesLA,0.0)
 
 head[0] = -20
 posH.positionMove(head)
-ttsSay('estoy identificando tu mano izquierda')
-while not posH.checkMotionDone():
+tts.say('estoy identificando tu mano izquierda')
+while not (posH.checkMotionDone() and tts.checkSayDone()):
     sleep(0.1)
 
 
 ra = list([-38.77, -9.79, -10.96, -63.36, -7.20, 8.00])
 posRA.positionMove(yarp.DVector(ra))
-ttsSay('voy, a, seguirla')
-while not posRA.checkMotionDone():
+tts.say('voy, a, seguirla')
+while not (posRA.checkMotionDone() and tts.checkSayDone()):
     sleep(0.1)
 
 
@@ -119,32 +123,32 @@ head[0] = -40
 posH.positionMove(head)
 ra = list([-60.00, -9.79, -20.96, -63.36, -7.20, 8.00])
 posRA.positionMove(yarp.DVector(ra))
-ttsSay('estas moviendo tu mano hacia arriba')
-while not posRA.checkMotionDone():
+tts.say('estas moviendo tu mano hacia arriba')
+while not (posRA.checkMotionDone() and tts.checkSayDone()):
     sleep(0.1)
 
 head[0] = -30
 posH.positionMove(head)
 ra = list([-60.39, -9.79, -10.96, -47.52, -7.19, 22.37])
 posRA.positionMove(yarp.DVector(ra))
-ttsSay('ahora la estas moviendo hacia abajo')
-while not posRA.checkMotionDone():
+tts.say('ahora la estas moviendo hacia abajo')
+while not (posRA.checkMotionDone() and tts.checkSayDone()):
     sleep(0.1)
 
 head[0] = -35
 posH.positionMove(head)
 ra = list([-50.77, -9.79, -20.96, -30.0, -7.20, 0.0])
 posRA.positionMove(yarp.DVector(ra))
-ttsSay('un poco más abajo')
-while not posRA.checkMotionDone():
+tts.say('un poco más abajo')
+while not (posRA.checkMotionDone() and tts.checkSayDone()):
     sleep(0.1)
 
 head[0] = -40
 posH.positionMove(head)
 ra = list([-28.57, -9.79, -20.96, -75.24, -7.19, 15.98])
 posRA.positionMove(yarp.DVector(ra))
-ttsSay('estas acercando tu mano izquierda a mi mano')
-while not posRA.checkMotionDone():
+tts.say('estas acercando tu mano izquierda a mi mano')
+while not (posRA.checkMotionDone() and tts.checkSayDone()):
     sleep(0.1)
 
 
@@ -152,16 +156,16 @@ head[0] = 0
 posH.positionMove(head)
 ra = list([-28.57, -9.79, -20.96, -75.24, -7.19, 15.98])
 posRA.positionMove(yarp.DVector(ra))
-ttsSay('ahora veo tu mano derecha')
-while not posRA.checkMotionDone():
+tts.say('ahora veo tu mano derecha')
+while not (posRA.checkMotionDone() and tts.checkSayDone()):
     sleep(0.1)
 
-ttsSay('calibrando sensores')
+tts.say('calibrando sensores')
 
 head[0] = 20
 posH.positionMove(head)
-ttsSay('sigo viendo tu mano derecha moverse')
-while not posH.checkMotionDone():
+tts.say('sigo viendo tu mano derecha moverse')
+while not (posH.checkMotionDone() and tts.checkSayDone()):
     sleep(0.1)
 
 # Left arm moving
@@ -169,24 +173,24 @@ head[0] = 35
 posH.positionMove(head)
 la = list([-38.77, 9.79, -10.96, -63.36, -7.20, 8.00])
 posLA.positionMove(yarp.DVector(la))
-ttsSay('voy, a, seguirla')
-while not posLA.checkMotionDone():
+tts.say('voy, a, seguirla')
+while not (posLA.checkMotionDone() and tts.checkSayDone()):
     sleep(0.1)
 
 head[0] = 40
 posH.positionMove(head)
 la = list([-60.00, 9.79, 20.96, -63.36, -7.20, 8.00])
 posLA.positionMove(yarp.DVector(la))
-ttsSay('estas moviendo tu mano derecha hacia arriba')
-while not posLA.checkMotionDone():
+tts.say('estas moviendo tu mano derecha hacia arriba')
+while not (posLA.checkMotionDone() and tts.checkSayDone()):
     sleep(0.1)
 
 head[0] = 30
 posH.positionMove(head)
 la = list([-60.00, 6.02, 5.42, -63.36, 7.19, 15.71])
 posLA.positionMove(yarp.DVector(la))
-ttsSay('estas moviendo tu mano hacia mi derecha')
-while not posLA.checkMotionDone():
+tts.say('estas moviendo tu mano hacia mi derecha')
+while not (posLA.checkMotionDone() and tts.checkSayDone()):
     sleep(0.1)
 
 
@@ -194,24 +198,24 @@ head[0] = 20
 posH.positionMove(head)
 la = list([-60.00, -2.02, 5.42, -63.36, 7.19, 15.74])
 posLA.positionMove(yarp.DVector(la))
-ttsSay('un poco mas a la derecha')
-while not posLA.checkMotionDone():
+tts.say('un poco mas a la derecha')
+while not (posLA.checkMotionDone() and tts.checkSayDone()):
     sleep(0.1)
 
 head[0] = 25
 posH.positionMove(head)
 la = list([-75.6, 2.00, 5.41, -63.36, 7.19, 36.66])
 posLA.positionMove(yarp.DVector(la))
-ttsSay('ahora la estas moviendo hacia arriba')
-while not posLA.checkMotionDone():
+tts.say('ahora la estas moviendo hacia arriba')
+while not (posLA.checkMotionDone() and tts.checkSayDone()):
     sleep(0.1)
 
 head[0] = 20
 posH.positionMove(head)
 la = list([-88.2, 1, 0.0, -20.87, 7.19, 1.74])
 posLA.positionMove(yarp.DVector(la))
-ttsSay('ahora la estas alejando')
-while not posLA.checkMotionDone():
+tts.say('ahora la estas alejando')
+while not (posLA.checkMotionDone() and tts.checkSayDone()):
     sleep(0.1)
 
 head[0] = 0
@@ -220,8 +224,8 @@ ra = list([-46.94, -24.47, 2.74, -53.46, 0.0, 0.0])
 la = list([-46.94, 24.47, 2.74, -53.46, 0.0, 0.0])
 posRA.positionMove(yarp.DVector(ra))
 posLA.positionMove(yarp.DVector(la))
-ttsSay('ahora veo tus dos manos. Esto es una locura ')
-while not (posLA.checkMotionDone() and posRA.checkMotionDone() ):
+tts.say('ahora veo tus dos manos. Esto es una locura ')
+while not (posLA.checkMotionDone() and posRA.checkMotionDone() and tts.checkSayDone()):
     sleep(0.1)
 
 # Home
@@ -231,4 +235,4 @@ la = yarp.DVector(axesLA,0.0)
 posH.positionMove(head)
 posRA.positionMove(ra)
 posLA.positionMove(la)
-ttsSay('creo que he tenido suficiente por hoy. Necesito descansar')
+tts.say('creo que he tenido suficiente por hoy. Necesito descansar')
